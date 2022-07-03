8f2ffb3aa1992a4fc70860957956c2901cf6df9c
issei <isseieikisouya@outlook.jp>
2022-07-03 20:40:38 +0900

    ホーム画面のモック製作開始

---
+++ chonchon_for_business/android/app/src/main/kotlin/com/example/chonchon_for_business/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chonchon_for_business
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chonchon_for_business/lib/view/home.dart
@@ -0,0 +1,135 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      home: const HomePage(title: "Home"),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(children: [EdgeRoundContainer("6/27"),EdgeRoundContainer("6/27")],),
+          UserCard(
+              "ダンスの振り付け一緒に考えませんか？",
+              "users/account1.jpg",
+              [EdgeRoundContainer("6/27"), EdgeRoundContainer("6/28")],
+              Eventcard("ダンス好き集まれ！", "users/account1.jpg")),
+              UserCard(
+              "ダンスミュージックについて語りたい！",
+              "users/account2.jpg",
+              [EdgeRoundContainer("6/27"), EdgeRoundContainer("6/28")],
+              Eventcard("ダンス好き集まれ！", "users/account2.jpg"))
+        ],
+      )),
+    );
+  }
+}
+
+Widget EdgeRoundContainer(String text) {
+  return (Container(
+      padding:EdgeInsets.all(5),
+      margin: EdgeInsets.all(3),
+      child: Text(text,style: TextStyle(color: Colors.white),),
+      decoration: BoxDecoration(
+          color: Colors.black.withAlpha(70), borderRadius: BorderRadius.circular(100))));
+}
+
+Widget Eventcard(String eventname, String eventimagepass) {
+  return (
+    Container(
+    
+    child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Container(
+            child: Row(
+          children: [
+            Container(
+              child: Image.asset(eventimagepass),
+              width: 50,
+              height: 50,
+            ),
+            Container(
+              padding:EdgeInsets.all(4),
+              child: Center(child: Text(eventname,style: TextStyle(color: Colors.white))),
+              height: 50,
+              color: Colors.black.withAlpha(70),
+            )
+          ],
+        ))),
+  ));
+}
+
+Widget UserCard(String message, String userimagepass, List<Widget> datewigets,
+    Widget eventcard) {
+  return (
+    
+    Container(
+      margin:EdgeInsets.all(10),
+     width: 420,
+     decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(20.0),boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0.0, 1.0),
+                    blurRadius: 6.0,
+                  ),
+                ],),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Container(
+            child: Row(
+          children: [
+            SizedBox(
+                child: Image.asset(userimagepass), height: 200, width: 200),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Container(margin: EdgeInsets.all(3), child: eventcard),
+                  Container(
+                      margin: EdgeInsets.all(3),
+                      child: Row(
+                        children: datewigets,
+                      )),
+                  Container(
+                    child: Text(message),
+                    width: 150,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: ElevatedButton(
+                      child: const Text('参加する！'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        shape: const StadiumBorder(),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )),
+      )));
+}

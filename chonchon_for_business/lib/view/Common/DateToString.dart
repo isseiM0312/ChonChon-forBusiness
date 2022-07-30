6b1685b594d6b100d8c6fd2cf29adc654190f6d0
Yuga <yugaKeio23@gmail.com>
2022-07-30 10:39:43 +0900

    Homeの作成

---
+++ chonchon_for_business/lib/view/Common/DateToString.dart
@@ -0,0 +1,40 @@
+class DateToString {
+  DateToString._();
+
+  static String monthToString(DateTime dateTime) {
+    if (dateTime.month.toString().length == 1) {
+      return "0" + dateTime.month.toString();
+    } else {
+      return dateTime.month.toString();
+    }
+  }
+
+  static String dayToString(DateTime dateTime) {
+    if (dateTime.day.toString().length == 1) {
+      return "0" + dateTime.day.toString();
+    } else {
+      return dateTime.day.toString();
+    }
+  }
+
+  static String weekdayToString(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return "月";
+      case 2:
+        return "火";
+      case 3:
+        return "水";
+      case 4:
+        return "木";
+      case 5:
+        return "金";
+      case 6:
+        return "土";
+      case 7:
+        return "日";
+      default:
+        return "";
+    }
+  }
+}
--- chonchon_for_business/lib/view/NavigationPages/Home.dart
+++ chonchon_for_business/lib/view/NavigationPages/Home.dart
@@ -1,9 +1,7 @@
-import 'dart:html';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-import 'package:cupertino_icons/cupertino_icons.dart';
+import '../Common/DateToString.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,7 +9,10 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home',
-      home: const HomePage(title: "Home"),
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: HomePage(title: "Home"),
     );
   }
 }
@@ -25,105 +26,234 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xffb94630),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                
+    return DefaultTabController(
+        initialIndex: 0,
+        length: 3,
+        child: Scaffold(
+            backgroundColor: Color(0xffb94630),
+            appBar: PreferredSize(
+                preferredSize: const Size.fromHeight(150),
+                child: AppBar(
+                  backgroundColor: Colors.black,
+                  title: const Text(
+                    '"Chon Chon"',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                  flexibleSpace: searchBox(),
+                  bottom: const TabBar(
+                    unselectedLabelStyle: TextStyle(),
+                    labelStyle: TextStyle(),
+                    tabs: <Widget>[
+                      Tab(
+                        text: "新着",
+                      ),
+                      Tab(
+                        text: "おすすめ",
+                      ),
+                      Tab(
+                        text: "フォロー",
+                      ),
+                    ],
+                  ),
+                )),
+            body: TabBarView(
+              children: <Widget>[
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          eventCard("ビブリオバトル", ["ビブリオバトル", "定期開催"],
+                              DateTime(2022, 7, 29, 13, 59), "読書", 2),
+                          eventCard("ビジネスに役立つ読書", ["経済", "ビジネス", "金融"],
+                              DateTime(2022, 7, 29, 14, 50), "読書", 3),
+                          eventCard("ひまわり図書館", ["図書館"],
+                              DateTime(2022, 7, 29, 13, 58), "読書", 1),
+                          eventCard("人文系の本を読もう！", ["初学者歓迎", "メディアセンター"],
+                              DateTime(2022, 7, 29, 14, 30), "読書", 5),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Center(),
+                Center(),
               ],
-            ),
-          ],
-        ),
-      ),
-    );
+            )));
   }
 }
 
-Widget SearchBox() {
+Widget searchBox() {
   return (Container(
-      width: 400,
-      margin: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(0.0, 1.0),
-            blurRadius: 6.0,
+      margin: const EdgeInsets.only(left: 30, top: 45, right: 30),
+      child: TextField(
+        cursorColor: Color(0xffb94630),
+        decoration: InputDecoration(
+          hintText: "検索",
+          fillColor: Colors.white,
+          filled: true,
+          isDense: true,
+          prefixIcon: const Icon(
+            CupertinoIcons.search,
           ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: Container(
-          child: Row(children: [
-            Container(
-              child: Icon(
-                Icons.search,
-                color: Colors.blueAccent,
-              ),
-              padding: EdgeInsets.all(7),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 20),
-              width: 200,
-              child: TextField(
-                decoration: InputDecoration(hintText: "Search"),
-              ),
-            )
-          ]),
+          contentPadding: const EdgeInsets.all(0),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(32),
+          ),
         ),
       )));
 }
 
-Widget EdgeRoundContainer(String text,double height,double witdh, Color color) {
-  return (Container(
-      height: height,
-      width: witdh,
-      padding: EdgeInsets.all(2),
-      margin: EdgeInsets.all(3),
-      child: Text(
-        text,
-        style: TextStyle(color: color),
-      ),
-      decoration: BoxDecoration(
-          color: Colors.black.withAlpha(70),
-          borderRadius: BorderRadius.circular(100))));
-}
-
-Widget UserCard(
-  String title,
-  List<Widget> tags,
-  String date,
-  Widget catecory,
-  List<String> userimagepass
+Widget eventCard(
+  String eventTitle,
+  List<String> tags,
+  DateTime eventDate,
+  String category,
+  int currentParticipantsNum,
 ) {
   return (Container(
-      margin: EdgeInsets.all(10),
-      height: 50,
-      width: 500,
+    margin: const EdgeInsets.all(6),
+    height: 85,
+    width: 500,
+    decoration: BoxDecoration(
+      color: Colors.white.withOpacity(0.55),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        staticEventInfo(
+          eventTitle,
+          tags,
+          eventDate,
+          category,
+        ),
+        dynamicEventInfo(
+          eventDate,
+          currentParticipantsNum,
+        ),
+      ],
+    ),
+  ));
+}
+
+Widget staticEventInfo(
+  String eventTitle,
+  List<String> tags,
+  DateTime eventDate,
+  String category,
+) {
+  return (Container(
+    padding: const EdgeInsets.only(left: 15),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              eventTitle,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(height: 3),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: tags.map((String e) => tagContainer("#" + e)).toList(),
+            ),
+            const SizedBox(height: 3),
+            Text(
+              DateToString.monthToString(eventDate) +
+                  "/" +
+                  DateToString.dayToString(eventDate) +
+                  "（" +
+                  DateToString.weekdayToString(eventDate) +
+                  "）",
+              style: const TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          ],
+        ),
+        categoryContainer(category),
+      ],
+    ),
+  ));
+}
+
+Widget tagContainer(String tag) {
+  return (Container(
+    margin: const EdgeInsets.only(right: 5),
+    padding: const EdgeInsets.only(left: 5, right: 5),
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(7),
+        color: Colors.white.withOpacity(0.8)),
+    child: Text(
+      tag,
+      style: const TextStyle(fontSize: 10),
+    ),
+  ));
+}
+
+Widget categoryContainer(String category) {
+  return (Container(
+      margin: const EdgeInsets.only(bottom: 2),
+      padding: const EdgeInsets.only(left: 5, right: 5),
       decoration: BoxDecoration(
-        color: Color(0xffb94630),
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      child:Row(children: [
-        Column(children: [
-          Container(child: Text(title),),
-          Container(child: Row(children: tags),),
-          Container(child: Text(date),),
-          Container(child: catecory)
-        ],),
-        Column(children: [
-          Container(child: Text("あと13分で募集終了!"),),
-          Container(height: 25,),
-          Container(child: Row(children: [Text("メンバー:2人"),],),)
-        ],)
-      ],)));
+          borderRadius: BorderRadius.circular(7), color: Color(0xffb94630)),
+      child: Row(
+        children: [
+          const Icon(
+            CupertinoIcons.scope,
+            color: Colors.black,
+            size: 10,
+          ),
+          Text(
+            category,
+            style: const TextStyle(color: Colors.white, fontSize: 12),
+          ),
+        ],
+      )));
+}
+
+Widget dynamicEventInfo(DateTime eventDate, int currentParticipantsNum) {
+  return (Container(
+    padding: const EdgeInsets.only(right: 15),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        timeLeft(eventDate),
+        Row(
+          children: [
+            Text("現在のメンバー: " + currentParticipantsNum.toString() + "人")
+          ],
+        ),
+      ],
+    ),
+  ));
+}
+
+Text timeLeft(DateTime eventDate) {
+  Duration timeLeft = eventDate.difference(DateTime.now());
+
+  if (timeLeft.inMinutes < 60 && timeLeft.inMinutes > 0) {
+    return Text(
+      "あと" + timeLeft.inMinutes.toString() + "分で募集終了！",
+      style: TextStyle(color: Color(0xffb94630)),
+    );
+  } else if (timeLeft.inMinutes < 1 && eventDate.isAfter(DateTime.now())) {
+    return const Text(
+      "まもなく募集終了！",
+      style: TextStyle(color: Color(0xffb94630)),
+    );
+  } else {
+    return const Text("");
+  }
 }
